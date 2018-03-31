6e5da3780f192acca98389ecfa4268c09263d451
amnesia <stefansdev@gmail.com>
2018-03-31 04:27:09 +0300

    Third page

--- app/js/common.js
+++ app/js/common.js
@@ -26,3 +26,17 @@ $('.main-menu_btn').click(function(e){
     $('.main-menu-list').slideToggle();
     $(this).toggleClass('__active');
   });
+
+  //Loader
+  $(window).load(function() {
+
+    $(".loader_inner").fadeOut();
+    $(".loader").delay(400).fadeOut("slow");
+  
+    $(".top_header").animated("fadeInDown", "fadeOut");
+    $(".tabs_header .wrapper").animated("flipInY", "fadeOut");
+    $(".profi_item").animated("fadeInRight", "fadeOut");
+    $(".s_pofi form").animated("zoomInRight", "fadeOut");
+    $(".s_back h3").animated("fadeInUp", "fadeOut");
+    $("section h2, footer h2, .contacts_top .tabs").animated("fadeInUp", "fadeOut");
+  });
